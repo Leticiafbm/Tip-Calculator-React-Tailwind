67f7cabd939e7348077291f5d466aca3710e2091
Letícia Moreira <161176220+Leticiafbm@users.noreply.github.com>
2025-04-15 06:10:08 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/Tip-Calculator-React-Tailwind/",
   plugins: [
     react(),
     tailwindcss(),
